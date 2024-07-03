38436fcf1fe7f6d1f0ae00a3841f06bb8b406d50
jujimenezp <jujimenezp@unal.edu.co>
2024-07-02 23:22:51 -0500

    Driver modified to include additional parameters

--- bin/driver.py
+++ bin/driver.py
@@ -5,8 +5,9 @@ from pandas import read_csv
 import subprocess
 
 T=300 #Temperature
-initial_parameters = read_csv("data/transition_rates.csv", header=None, sep='    ', engine='python')
-params=[str(i) for i in initial_parameters.iloc[:,1]]
+transition_rates = read_csv("data/transition_rates.csv", header=None, sep='    ', engine='python')
+parameters = read_csv("data/parameters.csv", header=None, sep='    ', engine='python')
+params=[str(i) for i in transition_rates.iloc[:,1]]+[str(i) for i in parameters.iloc[:,1]]
 
 subprocess.run(['g++','--std=c++20','-Wall','src/main.cpp','-o','bin/main.x'])
 subprocess.run(['./bin/main.x',*params])
