f892e7b79c22de2c783687c9c2c1508aa123986a
jujimenezp <jujimenezp@unal.edu.co>
2024-07-08 19:09:23 -0500

    First attempt for class W_matrix

--- src/W_matrix.hpp
+++ src/W_matrix.hpp
@@ -12,7 +12,8 @@ class W_matrix{
     private:
     public:
         //Parameteres
-        double T,V,F,R,FV_RT;
+        double T,V,F,R;
+        double FVoRT;
 
         // Initial transition rates
         double k_1, k_2fv0, k_2rv0, ko_dN1v0, ko_bN1v0, \
@@ -27,33 +28,37 @@ class W_matrix{
         double c_Na_out, c_Na_in, c_K_out, c_K_in;
 
         //W transition matrix
-        Eigen::MatrixXd W = Eigen::MatrixXd::Zero(19,19);
+        Eigen::MatrixXd W;
 
-        W_matrix(double T, double V, double F, double R, double FV_RT, double  k_1, \
-                 double  k_2fv0, double  k_2rv0, double  ko_dN1v0, double  ko_bN1v0, \
+        W_matrix(double T, double V, double F, double R, \
+                 double c_Na_out, double c_Na_in, double c_K_out, double c_K_in, \
+                 double  k_1, double  k_2fv0, double  k_2rv0, double  ko_dN1v0, double  ko_bN1v0, \
                  double ko_dNv0, double  ko_bNv0, double  ko_dKv0, double  ko_bKv0, \
                  double  k_31, double k_32, double  k_4f, double  k_4r, double  ki_dN1v0, \
-                 double  ki_bN1v0, double ki_dN, double  ki_bN, double  ki_dK, double  ki_bk,\
-                 double c_Na_out, double c_Na_in, double c_K_out, double c_K_in)
+                 double  ki_bN1v0, double ki_dN, double  ki_bN, double  ki_dK, double  ki_bk)
         {
-            T=T; V=V; F=F; R=R; FV_RT=FV_RT;
+            T=T; V=V; F=F; R=R;
             k_1=k_1; k_2fv0=k_2fv0; k_2rv0=k_2rv0; ko_dN1v0=ko_dN1v0; ko_bN1v0=ko_bN1v0;
             ko_dNv0=ko_dNv0; ko_bNv0=ko_bNv0; ko_dKv0=ko_dKv0; ko_bKv0=ko_bKv0;
             k_31=k_31; k_32=k_32; k_4f=k_4f; k_4r=k_4r; ki_dN1v0=ki_dN1v0;
             ki_bN1v0=ki_bN1v0; ki_dN=ki_dN; ki_bN=ki_bN; ki_dK=ki_dK; ki_bk=ki_bk;
 
-            k_2f = k_2fv0*exp(0.1*FV_RT);
-            k_2r = k_2rv0*exp(-0.1*FV_RT);
-            ko_dN1 = ko_dN1v0*exp(0.65*FV_RT);
-            ko_bN1 = ko_bN1v0*exp(-0.65*FV_RT);
-            ko_dN = ko_dNv0*exp(0.185*FV_RT);
-            ko_bN = ko_bNv0*exp(-0.185*FV_RT);
-            ko_dK = ko_dKv0*exp(0.185*FV_RT);
-            ko_bK = ko_bKv0*exp(-0.185*FV_RT);
-            ki_dN1 = ki_dN1v0*exp(-0.25*FV_RT);
-            ki_bN1 = ki_bN1v0*exp(0.25*FV_RT);
+            FVoRT = F*V/(R*T);
+
+            k_2f = k_2fv0*std::exp(0.1*FVoRT);
+            k_2r = k_2rv0*std::exp(-0.1*FVoRT);
+            ko_dN1 = ko_dN1v0*std::exp(0.65*FVoRT);
+            ko_bN1 = ko_bN1v0*std::exp(-0.65*FVoRT);
+            ko_dN = ko_dNv0*std::exp(0.185*FVoRT);
+            ko_bN = ko_bNv0*std::exp(-0.185*FVoRT);
+            ko_dK = ko_dKv0*std::exp(0.185*FVoRT);
+            ko_bK = ko_bKv0*std::exp(-0.185*FVoRT);
+            ki_dN1 = ki_dN1v0*std::exp(-0.25*FVoRT);
+            ki_bN1 = ki_bN1v0*std::exp(0.25*FVoRT);
 
             c_Na_out=c_Na_out; c_Na_in=c_Na_in; c_K_out=c_K_out; c_K_in=c_K_in;
+
+            W = Eigen::MatrixXd::Zero(19,19);
 
             W(0,0) = -(k_1+ki_dN1); W(0,13) = ki_bN1*c_Na_in;
             W(1,0) = k_1; W(1,1) = -k_2f; W(1,2) = k_2r;
@@ -81,21 +86,45 @@ class solver{
     private:
     public:
         Eigen::EigenSolver<Eigen::MatrixXd> solver;
-        void initialize(Eigen::MatrixXd &W){solver = Eigen::EigenSolver<Eigen::MatrixXd>(W);}
-        Eigen::VectorXd get_eigenvalues(Eigen::MatrixXd &W){return solver.eigenvalues().real();}
-        Eigen::MatrixXd get_eigenvectors(Eigen::MatrixXd &W){return solver.eigenvectors().real();}
-        int steady_state_index(Eigen::VectorXd &eigenvalues, double threshold){
-            try{
-                for(int i=0;i < eigenvalues.size(); i++){
-                    if(fabs(eigenvalues[i]) < threshold){return i;}
-                }
-                throw (9999);
+        void initialize(const Eigen::MatrixXd &W){solver = Eigen::EigenSolver<Eigen::MatrixXd>(W);}
+        Eigen::VectorXd get_eigenvalues(const Eigen::MatrixXd &W){return solver.eigenvalues().real();}
+        Eigen::MatrixXd get_eigenvectors(const Eigen::MatrixXd &W){return solver.eigenvectors().real();}
+
+        // Find index of eigenvalue for the steady state.
+        // Does not work correctly if there are many values under the given threshold.
+        int steady_state_index(Eigen::VectorXd &eigenvalues, double threshold=1e-12);
+
+        // Normalize a vector
+        Eigen::VectorXd normalize_vector(const Eigen::VectorXd v){return v/v.sum();}
+
+        // Normalize every column of a matrix
+        Eigen::MatrixXd normalize_columns(const Eigen::MatrixXd W){
+            Eigen::MatrixXd result = Eigen::MatrixXd::Zero(W.rows(),W.cols());
+
+            for(int i=0; i < W.cols(); i++){
+                result.col(i) = W.col(i) / (W.col(i)).sum();
             }
-            catch(int error){
-                std::cout << "No eigenvalue found with absolute value under " << threshold << std::endl;
-                exit(1);
-            }
+            return result;
         }
+
+        // Calculate current J_ji going from state i to state j.
+        double get_current(Eigen::MatrixXd &W, Eigen::VectorXd &v, int i, int j);
 };
 
+int solver::steady_state_index(Eigen::VectorXd &eigenvalues, double threshold){
+    try{
+        for(int i=0;i < eigenvalues.size(); i++){
+            if(fabs(eigenvalues[i]) < threshold){return i;}
+        }
+        throw (9999);
+    }
+    catch(int error){
+        std::cout << "No eigenvalue found with absolute value under " << threshold << std::endl;
+        exit(1);
+    }
+}
+
+double solver::get_current(Eigen::MatrixXd &W, Eigen::VectorXd &v, int i, int j){
+    return W(j,i)*v[i]-W(i,j)*v[j];
+}
 #endif // W_MATRIX_H_
