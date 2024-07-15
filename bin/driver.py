c3859dca15a56ddcbd73797ab94eb278b8d82954
jujimenezp <jujimenezp@unal.edu.co>
2024-07-15 15:54:24 -0500

    Driver adjusted for changes in parameters and transition_rates files

--- bin/driver.py
+++ bin/driver.py
@@ -4,12 +4,12 @@
 from pandas import read_csv
 import subprocess
 
-main_cpp = "src/15_states.cpp"
-main_exe = "bin/15_states.x"
+main_cpp = "src/19_states.cpp"
+main_exe = "bin/19_states.x"
 flags_cpp = ['--std=c++20', '-Wall']
 
-transition_rates = read_csv("data/transition_rates.csv", header=None, sep='    ', engine='python')
-parameters = read_csv("data/parameters.csv", header=None, sep='    ', engine='python')
+transition_rates = read_csv("data/transition_rates.csv", skiprows=0, sep=',')
+parameters = read_csv("data/parameters.csv", skiprows=0, sep=',')
 params=[str(i) for i in parameters.iloc[:,1]]+[str(i) for i in transition_rates.iloc[:,1]]
 
 print('Deleting '+main_exe)
@@ -21,6 +21,7 @@ subprocess.run(['g++',*flags_cpp,main_cpp,'-o',main_exe])
 print('Running '+main_exe+'...')
 try:
     subprocess.run([main_exe,*params])
+    print('Results saved in results directory.')
 except Exception as e:
     print(e)
     print('Could not run '+main_exe+'. Check runtime errors.')
