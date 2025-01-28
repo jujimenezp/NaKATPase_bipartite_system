b401e673137f3268328f9c0bde79a56a54fea73d
jujimenezp <jujimenezp@unal.edu.co>
2025-01-28 09:09:18 -0500

    Refined driver for different uses

--- bin/driver.py
+++ bin/driver.py
@@ -2,6 +2,7 @@
 
 # Script to read transition rates and pass them to main program
 from pandas import read_csv
+from numpy import linspace
 import subprocess
 import sys
 
@@ -14,16 +15,43 @@ transition_rates = read_csv("data/transition_rates.csv", skiprows=0, sep=',')
 parameters = read_csv("data/parameters.csv", skiprows=0, sep=',')
 params=[str(i) for i in transition_rates.iloc[:,1]]+[str(i) for i in parameters.iloc[:,1]]
 
-print('Deleting '+main_exe)
-subprocess.run(['rm',main_exe],stderr=subprocess.DEVNULL)
+def regular_exe():
+    print('Deleting '+main_exe)
+    subprocess.run(['rm',main_exe],stderr=subprocess.DEVNULL)
 
-print('Compiling '+main_cpp+'...')
-subprocess.run(['g++',*flags_cpp,main_cpp,'-o',main_exe])
+    print('Compiling '+main_cpp+'...')
+    subprocess.run(['g++',*flags_cpp,main_cpp,'-o',main_exe])
 
-print('Running '+main_exe+'...')
-try:
-    subprocess.run([main_exe,*params])
-    print('Results saved in results directory.')
-except Exception as e:
-    print(e)
-    print('Could not run '+main_exe+'. Check runtime errors.')
+    print('Running '+main_exe+'...')
+    try:
+        subprocess.run([main_exe,*params])
+        print('Results saved in results directory.')
+    except Exception as e:
+        print(e)
+        print('Could not run '+main_exe+'. Check runtime errors.')
+
+def neuron_voltage_range():
+    voltage_range = linspace(-87e-3,60e-3,100)
+    print(voltage_range)
+    with open('results/voltage_range.dat', 'w') as f:
+        print("voltage(V)\tturnover(1/s)\tWork(kBT/cycle)\tHeat(kBT/cycle)\tQdot_x\tQdot_y\tWdot_x\tWdot_y\tIdot_x\tIdot_y\tEfficiency", file=f)
+
+    print('Deleting '+main_exe)
+    subprocess.run(['rm',main_exe],stderr=subprocess.DEVNULL)
+
+    print('Compiling '+main_cpp+'...')
+    subprocess.run(['g++',*flags_cpp,main_cpp,'-o',main_exe])
+
+    for v in voltage_range:
+        params[20] = str(v)
+        print(v)
+        print('Running '+main_exe+'...')
+        try:
+            subprocess.run([main_exe,*params])
+            print('Results saved in results directory.')
+        except Exception as e:
+            print(e)
+            print('Could not run '+main_exe+'. Check runtime errors.')
+
+if __name__=='__main__':
+    neuron_voltage_range()
